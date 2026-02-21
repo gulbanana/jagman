3264127d231d0d50da9557af3c85371ab3de5b48
Thomas Castiglione <castiglione@mac.com>
2026-02-21 22:30:09 +0800

    EXTREMELY fancy logo

--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -1,23 +1,31 @@
 <div class="layout">
     <div style="grid-area: J">J</div>
-    <div class="suffix">u</div>
-    <div class="suffix">j</div>
-    <div class="suffix">u</div>
-    <div class="suffix">t</div>
-    <div class="suffix">s</div>
-    <div class="suffix">u</div>
+    <div style="grid-area: uju" class="suffix column3">
+        <div>u</div>
+        <div>j</div>
+        <div>u</div>
+    </div>
+    <div style="grid-area: tsu" class="suffix column3">
+        <div>t</div>
+        <div>s</div>
+        <div>u</div>
+    </div>
     <div style="grid-area: A">A</div>
     <div style="grid-area: G">G</div>
-    <div class="suffix" style="grid-row: 2">e</div>
-    <div class="suffix">n</div>
-    <div class="suffix">t</div>
+    <div style="grid-area: ent" class="suffix column3">
+        <div>e</div>
+        <div>n</div>
+        <div>t</div>
+    </div>
     <div style="grid-area: M">M</div>
     <div style="grid-area: B">A</div>
     <div style="grid-area: N">N</div>
-    <div class="suffix" style="grid-row: 3">a</div>
-    <div class="suffix" style="grid-row: 3">g</div>
-    <div class="suffix" style="grid-row: 3">e</div>
-    <div class="suffix" style="grid-row: 3">r</div>
+    <div style="grid-area: ager" class="suffix column4">
+        <div>a</div>
+        <div>g</div>
+        <div>e</div>
+        <div>r</div>
+    </div>
 </div>
 
 <style>
@@ -25,19 +33,41 @@
         margin: 8px 0;
 
         font-family: var(--stack-code);
-        font-size: 24px;
+        font-size: 32px;
 
         display: grid;
         grid-template-areas:
-            "J . . . . . ."
-            "A G . . . . ."
-            "M B N . . . .";
-        grid-template-columns: repeat(7, 16px);
-        grid-template-rows: 24px 24px 24px;
+            "J uju tsu ."
+            "A G   ent ."
+            "M B   N   ager";
+        grid-template-columns: repeat(4, 24px);
+        grid-template-rows: 32px 32px 32px;
     }
 
-    .suffix {
-        color: var(--ctp-overlay1);
-        /* font-weight: lighter; */
+    .column3 {
+        font-size: 12px;
+        display: grid;
+        align-content: center;
+        grid-template-columns: auto auto auto;
+    }
+
+    .column4 {
+        font-size: 12px;
+        padding-top: 3px;
+        display: grid;
+        grid-template-rows: auto auto;
+        grid-template-columns: auto auto;
+        & > :nth-child(0n + 2) {
+            grid-row: 1;
+            grid-column: 2;
+        }
+        & > :nth-child(0n + 3) {
+            grid-row: 2;
+            grid-column: 1;
+        }
+        & > :nth-child(0n + 4) {
+            grid-row: 2;
+            grid-column: 2;
+        }
     }
 </style>
